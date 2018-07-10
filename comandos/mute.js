fc49766f318adc6b748c4588c7625ffd6672c1ee
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-07-09 23:31:37 -0300

    Create mute.js
    O

---
+++ comandos/mute.js
@@ -0,0 +1,74 @@
+message.delete().catch(O_o=>{});
+   if (message.member.hasPermission('MANAGE_MESSAGES')) {
+   const comousar = new Discord.RichEmbed()
+      .setAuthor("BattleBOT", client.user.avatarURL)
+      .setTitle("k!mute")
+      .setDescription(`Ira mutar o usuário mencionado.`)
+      .setColor("#60d1f6")
+      .setFooter("© BattleBOT - Luam Copyright")
+      .addField("Como usar", "`k!mute @usuário <motivo>`")
+      .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Gerenciar mensagens`")
+
+   let member = message.mentions.members.first();
+   if(!member)
+      return message.channel.send(comousar);
+
+   let motivo = args.slice(1).join(' ');
+   if(!motivo)
+      return message.reply("Por favor, indique um motivo para o mute!");
+
+   if (!message.guild.roles.find("name", "Mutado") || message.guild.roles.find("name", "mutado")) {
+      
+      const norole = new Discord.RichEmbed()
+         .setColor("ff0000")
+         .setAuthor('Deu um erro', client.user.avatarURL)
+         
+         .setDescription(`${message.author}, o cargo **Mutado** não foi encontrado. :slight_frown: 
+Crie um cargo com o nome "**Mutado**", assim poderei mutar o usuário!`)
+      
+      
+         .setTimestamp()
+         .setFooter("© BattleBOT???? ERRO", message.author.avatarURL)
+      
+      message.channel.send(norole)
+      
+      
+     } else {
+        let role = message.guild.roles.find("name", "Mutado");
+        member.addRole(role)
+        
+        const mutemsg = new Discord.RichEmbed()
+          .setAuthor('Você foi mutado!', member.user.avatarURL)
+          .setColor("ff0000")
+
+          .setThumbnail(member.user.avatarURL)
+
+          .setTimestamp()
+          .setFooter("© BattleBOT???? Moderação", message.author.avatarURL)
+
+          .addField("Motivo:", motivo)
+
+          .addField("Servidor:", message.guild.name)  
+  
+       member.send(mutemsg)
+        
+        const mutado = new Discord.RichEmbed()
+           .setAuthor(member.user.tag + ' | Mute', member.user.avatarURL)
+           .setDescription(`${member.user.tag} (ID: ${member.user.id}) não respeitou as regras e foi mutado! :pensive: `)
+           .setColor("ff0000")
+
+           .setThumbnail(message.author.avatarURL)
+
+           .setTimestamp()
+           .setFooter("© BattleBOT???? Moderação", message.author.avatarURL)
+
+           .addField("Motivo:", motivo)
+
+           .addField("Staffer:", message.author)
+        
+        message.channel.send(mutado)
+     }
+  
+   } else {
+      message.reply("você não tem permissão! :x:")
+  }
