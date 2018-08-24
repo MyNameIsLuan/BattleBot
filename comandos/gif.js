8d8d00bc23f4bf572b2ee881ae8cb8d1214c4623
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-08-24 20:49:16 -0300

    Create gif.js

---
+++ comandos/gif.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const gifSearch = require("gif-search");
+
+module.exports.run = async (L, message, args) => {
+
+    let pesq = args.join(" ")
+    gifSearch.random(pesq).then(
+    gifUrl => message.channel.send({embed: {
+        title: `Resultado da pesquisa de gifs: ${pesq}`,
+        image: {url: gifUrl}
+        
+    }})
+);
+}
+
+module.exports.help = {
+    name: "gif"
+}
