8cce91d82f223f3e6ccc4e2bf0c6c0556ecf97de
MyNameIsLuan <40917768+MyNameIsLuan@users.noreply.github.com>
2018-08-24 20:44:23 -0300

    Update anunciar.js

--- comandos/anunciar.js
+++ comandos/anunciar.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{})
-   if(!message.member.id == "368111860735541248") {
+   if(message.member.id != "368111860735541248") {
         message.delete();
         message.reply("Apenas o ImLuan pode usar este comando!");
         return;
